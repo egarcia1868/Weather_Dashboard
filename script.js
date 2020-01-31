925ecb740aa352cde60678a1bd0cef0a477c2aa2
Eric Garcia <egarcia1868@gmail.com>
2020-01-30 18:19:54 -0600

    changing the html to pull the images from a better looking source

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ $("#city").submit(function(event){
       searchedCitiesAr.push($("#cityName").val().trim());
   };
   getThatWeather($("#cityName"));
+  $("#city")[0].reset();
 });
 
 $(document).on("click", ".prevCity", function() {getThatWeather(event.target)});
@@ -40,7 +41,7 @@ $(document).on("click", ".prevCity", function() {getThatWeather(event.target)});
     var wind = Math.round(response.wind.speed);
     $("#currentCityName").html(`${response.name} <span id="currentDate"></span> <span id="weather"></span>`);
     $("#currentDate").html(moment().format("(M/D/YYYY)"));
-    $("#weather").html(`<img id="weather" src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" alt="${response.weather[0].description}">`);
+    $("#weather").html(`<img id="weather" src="https://openweathermap.org/img/w/${response.weather[0].icon}.png" alt="${response.weather[0].description}">`);
     $("#temp").text(`${temp}°F`);
     $("#humid").text(`${response.main.humidity}%`);
     $("#wind").text(`${wind} MPH`);
@@ -63,6 +64,8 @@ $(document).on("click", ".prevCity", function() {getThatWeather(event.target)});
       };
       $("#uv").text(uv);
     });
+
+    // This will create and populate my 5 day forecast
     $.ajax({
       url: `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=c3da05bca4528c3489ab056a709792c2&units=imperial`,
       method: "GET"
@@ -71,11 +74,12 @@ $(document).on("click", ".prevCity", function() {getThatWeather(event.target)});
       // var hourGroup;
       // if (currentHour.substr(id.length - 8))
       for (var i = 0; i < 5; i++) {
-      var temp = Math.round(forecastResponse.list[6 + (i * 8)].main.temp);
-      $(`#card${i+1}Date`).html(moment().add((i+1), 'day').format("(M/D/YYYY)"));
-      $(`#card${i+1}Icon`).html(`<img class="forecastIcon" src="https://openweathermap.org/img/wn/${forecastResponse.list[6 + (i * 8)].weather[0].icon}@2x.png" alt="${forecastResponse.list[6 + (i * 8)].weather[0].description}">`);
-      $(`#card${i+1}Temp`).text(temp);
-      $(`#card${i+1}Humid`).text(forecastResponse.list[6 + (i * 8)].main.humidity);
+        $(".hide").removeClass("hide");
+        var temp = Math.round(forecastResponse.list[6 + (i * 8)].main.temp);
+        $(`#card${i+1}Date`).html(moment().add((i+1), 'day').format("(M/D/YYYY)"));
+        $(`#card${i+1}Icon`).html(`<img class="forecastIcon" src="https://openweathermap.org/img/w/${forecastResponse.list[6 + (i * 8)].weather[0].icon}.png" alt="${forecastResponse.list[6 + (i * 8)].weather[0].description}">`);
+        $(`#card${i+1}Temp`).text(temp);
+        $(`#card${i+1}Humid`).text(forecastResponse.list[6 + (i * 8)].main.humidity);
       }
     })
   })
